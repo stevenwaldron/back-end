7d3023b2d0dcd2c495a717462c076a069036332b
Steven Waldron <stevenwaldron@stevens-mbp.attlocal.net>
2020-08-20 18:08:08 -0700

    first commit

---
+++ routers/usersRouter.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+const usersRouter = express.router()
+
+usersRouter.get(path,(req,res)=> {
+  
+})
+
+usersRouter.get(path,(req,res)=> {
+  
+})
+
+usersRouter.post(path,(req,res)=> {
+  
+})
+
+usersRouter.put(path,(req,res)=> {
+  
+})
+
+usersRouter.delete(path,(req,res)=> {
+  
+})
+
+module.exports = usersRouter
+
+
+
+
+
